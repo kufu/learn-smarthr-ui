c1c4ea857292770748c5439ed8b0f181506a1c0f
shinichi.kogiso <kuusai1998@gmail.com>
2023-03-23 09:56:17 +0900

    途中までアップ

--- src/components/MDXComponents.tsx
+++ src/components/MDXComponents.tsx
@@ -1,7 +1,12 @@
 import { MDXProvider } from '@mdx-js/react'
 import { AppProps } from 'next/app'
+import { Button } from 'smarthr-ui'
+
+import { MyComponents } from './MyComponents'
 
 export const MDXComponents = {
+  MyComponents,
+  Button,
   // ここにカスタムコンポーネントを追加
   // 例: h1: (props) => <h1 style={{ color: 'red' }} {...props} />,
 }
