6662f6881d7b252734b1ca91cc2a08badfaeb475
uknmr <y.kinmori@gmail.com>
2023-04-10 15:57:52 +0900

    💬: 序文を見直した

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import fs from 'fs'
 import path from 'path'
 
 import { GetStaticProps } from 'next'
+import { BaseColumn, WarningIcon } from 'smarthr-ui'
 
 import { TextLink } from '@/features/ui'
 
@@ -14,8 +15,43 @@ type Post = {
 const Archive: React.FC<{ posts: Post[] }> = ({ posts }) => {
   return (
     <main>
-      <h1>SmartHR UI</h1>
-      <p>すべての “働く” を支えるためのコンポーネントライブラリ</p>
+      <h1>（仮）SmartHR UI を学ぼう</h1>
+      <p>ようこそ! ここは SmartHR UI の使い方を学ぶためのドキュメントサイトです。</p>
+
+      <h2>SmartHR UI について</h2>
+      <p>
+        SmartHR UI は、SmartHR が開発した、React ベースの UI コンポーネントライブラリです。このライブラリには、一般的な UI
+        要素だけでなく、業務アプリケーションに特化したコンポーネントも含まれています。開発者はこれらのコンポーネントを組み合わせることで、短期間で複雑な業務アプリケーションを構築できます。
+      </p>
+
+      <ul>
+        <li>
+          <TextLink href="https://smarthr.design/products/components/">コンポーネント｜SmartHR Design System</TextLink>
+        </li>
+        <li>
+          <TextLink href="https://github.com/kufu/smarthr-ui">SmartHR UI｜GitHub - kufu/smarthr-ui</TextLink>
+        </li>
+      </ul>
+
+      <h2>このサイトの目的</h2>
+
+      <p>
+        このサイトでは、SmartHR UI
+        を使った画面の作り方を、実際にブラウザの上で手を動かしながら学べます。ちょっとした見た目の変更から複雑なレイアウト構築まで少しずつ解説します。
+      </p>
+
+      <p>
+        最後まで読み進めることで、エンジニアやデザイナーでない方でも SmartHR UI
+        を使って画面を作るために必要な知識をすべて身につけることができるでしょう。
+      </p>
+
+      <BaseColumn>
+        <p>
+          <WarningIcon text="このサイトは HTML や CSS、JavaScript、React の基礎的な知識を前提としています。" />
+        </p>
+      </BaseColumn>
+
+      <h2>目次</h2>
       <ul>
         {posts.map(({ slug, title }) => (
           <li key={slug}>
